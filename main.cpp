0d1ece31896cf5aa4eb099d759f41eabdd4b5cb5
Parad0x763 <db726825@gmail.com>
2026-03-11 21:20:48 -0400

    Covered some information to changes done to the chrono stl, added some more documentation, and added some more notes to the readme.md

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <format>
 #include <iostream>
+#include <chrono>
 
 /* 
  * The following `max` function is intended to determine the max value between to different types
@@ -15,9 +17,63 @@ auto maxPrior( T x, U y ) {
     return ( x < y ) ? y : x;
 }
 
+/*
+ * Using the `chrono` library
+ * Get the current system time using `system_clock::new()`
+ * This was an older way of getting current time
+ */
+std::string getCurrentDateAndTime() {
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t( now );
+    std::stringstream stringStream;
+    // Y = year, m = month, d = day, X = hours and minutes
+    stringStream << std::put_time( std::localtime( &in_time_t ), "%Y-%m-%d %X" );
+
+    return stringStream.str();
+}
+
+/*
+ * C++20 way of getting current time
+ */
+std::string getCurrentDateAndTime20() {
+    auto const time = std::chrono::current_zone()->to_local( std::chrono::system_clock::now() );
+    return std::format( "{:%Y-%m-%d %X}", time );
+}
+
 int main () {
     std::cout << __cplusplus << std::endl;
-
+    /*
+     * Get the middle of the year date
+     */
+    constexpr auto middle { std::chrono::June / std::chrono::last };
+    std::cout << middle << std::endl;
+    /*
+     * Get the last Tuesday of the current year
+     */
+    constexpr auto ymwdl { std::chrono::year( 2026 ) / std::chrono::December / std::chrono::Tuesday[ std::chrono::last ] };
+    std::cout << ymwdl.month() << ymwdl.weekday() << ymwdl.year() << std::endl;
+    /*
+     * Showing the current day and time from the two funtions below
+     */
+    std::cout
+        << "Older: " << getCurrentDateAndTime()
+        << "\nNewer: " << getCurrentDateAndTime20()
+        << std::endl;
+    /*
+     * Gets the local timezone using `std::chrono::get_tzdb().current_zone()`
+     * tzdb represents a copy of the IANA time zone database
+     * `localTimezone` contains the following member functions
+     * name(), get_info(), to_sys(), to_local()
+     */
+    const std::chrono::time_zone* localTimezone = std::chrono::get_tzdb().current_zone();
+    std::cout << localTimezone->name() << std::endl;
+    /*
+     * Demonstrating using `Abbreviated Template Functions`
+     * `max20` uses `Abbreviated Template Functions` using the `auto` keyword
+     * `maxPrior` uses `template < typedef T, typedef U >`
+     * The main advantage of `max20` is that you could also pass in two of the same type
+     * such as two int's
+     */
     int tValue = 19;
     double uValue = 19.19;
 
--- readme.md
+++ readme.md
@@ -15,4 +15,38 @@
 
 ## C++20
 
+- [CPPReference](https://en.cppreference.com/index.html)
 - [Intro-to-C++20](https://www.learncpp.com/cpp-tutorial/introduction-to-c20/)
+
+### Abbreviated Function Templates
+
+- Allow for shorthand generic type definition in functions by using `auto`
+- Provides more robust use cases
+  - For example with the previous way
+```
+  template< typedef T, typedef U >
+  T max( T x, U y ) {
+    return ( x < y ) ? y : x;
+  }
+```
+  - The `max` function would error when passing `x` as an `int` and `y` as a `double` because the compiler does not handle type conversion automatically with templates
+- Using the `Abbreviated Function Templates` approach the previous issue is no longer a problem
+```
+  auto max( auto x, auto y ) {
+    return ( x < y ) ? y : x;
+  }
+```
+
+### Chrono Library ( originally introduced in C++11 )
+
+- [Chrono](https://en.cppreference.com/w/cpp/chrono.html)
+  - This site shows which standard introduced which feature / class / struct
+
+#### Main Types Added
+
+- [calendar](https://en.cppreference.com/w/cpp/chrono.html#Calendar) and [time zone](https://en.cppreference.com/w/cpp/chrono.html#Time_zone) where added
+
+#### Calendar
+
+- Adds many classes for getting expected calendar dates / functionality
+- 
